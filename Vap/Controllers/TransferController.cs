fa51559963ecc54f3e39bf57eaa2f7d7577d7c6c
Alessandro Littera <46992286+AlessandroLittera@users.noreply.github.com>
2019-09-04 23:59:48 +0200

    dasd

--- Vap/Controllers/TransferController.cs
+++ Vap/Controllers/TransferController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Models.Interfaces.Helpers;
+using Vap.Models;
 
 namespace Vap.Controllers
 {
     public class TransferController : Controller
     {
+        readonly IRequesttHelper requesttHelper;
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult RequestTrasf()
         {
+            return View();
+        }
+        public async Task<IActionResult> ListRequest()
+        {
+            //ICollection<Richieste> list = await accountHelper.AccountsAsync();
             return View();
         }
     }
